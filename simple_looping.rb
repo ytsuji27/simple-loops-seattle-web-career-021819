8b14c5ce6d021d464aeecbee270ae1022aec68ee
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-08 06:58:25 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,11 +21,11 @@ end
   
 #end
 
-#def times_iterator(number_of_times)
+def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   #phrase = "Welcome to Flatiron School's Web Development Course!"
   
-#end
+end
 
 #def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
