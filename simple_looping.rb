640ef524025f5031abacee08a5182eec3c60fad6
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-08 06:52:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,8 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
+#def loop_iterator(number_of_times)
 
-end
+#end
   
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
@@ -13,33 +13,33 @@ end
   #  phrase and break when the counter hits the appropriate number...
   #phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+#end
 
-def times_iterator(number_of_times)
+#def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   #phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+#end
 
-def while_iterator(number_of_times)
+#def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   #phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+#end
 
-def until_iterator(number_of_times)
+#def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   #phrase = "Welcome to Flatiron School's Web Development Course!"
 
-end
+#end
 
-def for_iterator(number_of_times)
+#def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   #phrase = "Welcome to Flatiron School's Web Development Course!"
   
-end
+#end
 
