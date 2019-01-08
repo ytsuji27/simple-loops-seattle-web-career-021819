558a615f8f626dac8990eb2cdb5503681db36fc2
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-08 06:55:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,9 @@ phrase = "Welcome to Flatiron School's Web Development Course!"
 def loop_iterator(number_of_times)
   loop do
     puts phrase
-    number_of_times =-
+    number_of_times =- 1
+    if number_of_times == 0
+      
 end
   
   # Code your solution here using the "loop" keyword to puts out the below phrase
